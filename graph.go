db09e32c38237ae10f5722a790b76650c5e7beb4
Rene Zbinden <rene.zbinden@postfinance.ch>
2015-11-02 16:39:41 +0100

    backup

--- graph.go
+++ graph.go
@@ -1,6 +1,10 @@
 package walker
 
-import "strings"
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
 
 type Dependency struct {
 	Dir  string
@@ -42,4 +46,117 @@ Loop:
 		graph[directory] = []string{}
 	}
 	return &graph
+}
+
+type graph map[string][]string
+
+func topSortDFS(g graph) (order, cyclic []string) {
+	L := make([]string, len(g))
+	i := len(L)
+	temp := map[string]bool{}
+	perm := map[string]bool{}
+	var cycleFound bool
+	var cycleStart string
+	var visit func(string)
+	visit = func(n string) {
+		switch {
+		case temp[n]:
+			cycleFound = true
+			cycleStart = n
+			return
+		case perm[n]:
+			return
+		}
+		temp[n] = true
+		for _, m := range g[n] {
+			visit(m)
+			if cycleFound {
+				if cycleStart > "" {
+					cyclic = append(cyclic, n)
+					if n == cycleStart {
+						cycleStart = ""
+					}
+				}
+				return
+			}
+		}
+		delete(temp, n)
+		perm[n] = true
+		i--
+		L[i] = n
+	}
+	for n := range g {
+		if perm[n] {
+			continue
+		}
+		visit(n)
+		if cycleFound {
+			return nil, cyclic
+		}
+	}
+	return L, nil
+}
+
+type Node struct {
+	Name  string
+	Edges []string
+}
+
+type Graph map[string][]string
+
+func (g *Graph) Contains(edges []string) bool {
+	m := *g
+	for node, _ := range m {
+		for _, edge := range edges {
+			if edge == node {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func bla(unsorted []Node) ([]string, error) {
+	unsorted_map := make(map[string][]string)
+	for _, n := range unsorted {
+		unsorted_map[n.Name] = n.Edges
+	}
+
+	sorted := []string{}
+	for {
+		if len(unsorted_map) == 0 {
+			break
+		}
+		acyclic := false
+		fmt.Println(unsorted_map)
+		for _, node := range unsorted {
+			if _, ok := unsorted_map[node.Name]; !ok {
+				continue
+			}
+			if contains(unsorted_map, node.Edges) {
+				acyclic = true
+				fmt.Println("remove" + node.Name)
+				delete(unsorted_map, node.Name)
+				sorted = append(sorted, node.Name)
+			}
+		}
+		if !acyclic {
+			return nil, errors.New("acyclic")
+		}
+	}
+	return sorted, nil
+}
+
+func contains(list map[string][]string, items []string) bool {
+	if len(items) == 0 {
+		return true
+	}
+	for itm, _ := range list {
+		for _, other := range items {
+			if itm == other {
+				return false
+			}
+		}
+	}
+	return true
 }
--- graph_test.go
+++ graph_test.go
@@ -8,116 +8,57 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-var files = `/aaa/eee/iii/kkkk
-/aaa/eee/iii/llll
-/aaa/eee/iii/mmmm
-/aaa/eee/jjj/kkkk
-/aaa/eee/jjj/llll
-/aaa/eee/jjj/mmmm
-/aaa/fff/iii/kkkk
-/aaa/fff/iii/llll
-/aaa/fff/iii/mmmm
-/aaa/fff/jjj/kkkk
-/aaa/fff/jjj/llll
-/aaa/fff/jjj/mmmm
-/aaa/ggg/iii/kkkk
-/aaa/ggg/iii/llll
-/aaa/ggg/iii/mmmm
-/aaa/ggg/jjj/kkkk
-/aaa/ggg/jjj/llll
-/aaa/ggg/jjj/mmmm
-/aaa/hhh/iii/kkkk
-/aaa/hhh/iii/llll
-/aaa/hhh/iii/mmmm
-/aaa/hhh/jjj/kkkk
-/aaa/hhh/jjj/llll
-/aaa/hhh/jjj/mmmm
-/bbb/eee/iii/kkkk
-/bbb/eee/iii/llll
-/bbb/eee/iii/mmmm
-/bbb/eee/jjj/kkkk
-/bbb/eee/jjj/llll
-/bbb/eee/jjj/mmmm
-/bbb/fff/iii/kkkk
-/bbb/fff/iii/llll
-/bbb/fff/iii/mmmm
-/bbb/fff/jjj/kkkk
-/bbb/fff/jjj/llll
-/bbb/fff/jjj/mmmm
-/bbb/ggg/iii/kkkk
-/bbb/ggg/iii/llll
-/bbb/ggg/iii/mmmm
-/bbb/ggg/jjj/kkkk
-/bbb/ggg/jjj/llll
-/bbb/ggg/jjj/mmmm
-/bbb/hhh/iii/kkkk
-/bbb/hhh/iii/llll
-/bbb/hhh/iii/mmmm
-/bbb/hhh/jjj/kkkk
-/bbb/hhh/jjj/llll
-/bbb/hhh/jjj/mmmm
-/ccc/eee/iii/kkkk
-/ccc/eee/iii/llll
-/ccc/eee/iii/mmmm
-/ccc/eee/jjj/kkkk
-/ccc/eee/jjj/llll
-/ccc/eee/jjj/mmmm
-/ccc/fff/iii/kkkk
-/ccc/fff/iii/llll
-/ccc/fff/iii/mmmm
-/ccc/fff/jjj/kkkk
-/ccc/fff/jjj/llll
-/ccc/fff/jjj/mmmm
-/ccc/ggg/iii/kkkk
-/ccc/ggg/iii/llll
-/ccc/ggg/iii/mmmm
-/ccc/ggg/jjj/kkkk
-/ccc/ggg/jjj/llll
-/ccc/ggg/jjj/mmmm
-/ccc/hhh/iii/kkkk
-/ccc/hhh/iii/llll
-/ccc/hhh/iii/mmmm
-/ccc/hhh/jjj/kkkk
-/ccc/hhh/jjj/llll
-/ccc/hhh/jjj/mmmm
-/ddd/eee/iii/kkkk
-/ddd/eee/iii/llll
-/ddd/eee/iii/mmmm
-/ddd/eee/jjj/kkkk
-/ddd/eee/jjj/llll
-/ddd/eee/jjj/mmmm
-/ddd/fff/iii/kkkk
-/ddd/fff/iii/llll
-/ddd/fff/iii/mmmm
-/ddd/fff/jjj/kkkk
-/ddd/fff/jjj/llll
-/ddd/fff/jjj/mmmm
-/ddd/ggg/iii/kkkk
-/ddd/ggg/iii/llll
-/ddd/ggg/iii/mmmm
-/ddd/ggg/jjj/kkkk
-/ddd/ggg/jjj/llll
-/ddd/ggg/jjj/mmmm
-/ddd/hhh/iii/kkkk
-/ddd/hhh/iii/llll
-/ddd/hhh/iii/mmmm
-/ddd/hhh/jjj/kkkk
-/ddd/hhh/jjj/llll
-/ddd/hhh/jjj/mmmm
+var files = `/application/orbit
+/application/deva
+/application/edm
+/application/oipf
+/application/adwh_tool
+/application/bmc_bppm_integration
+/db/oracle_ogg
+/db/sqoop
+/db/postgresql
+/db/postgresql_smdb
+/db/oracle_ggs
+/db/hive
+/group/accesszone_dualhomed
+/group/kfzv
+/group/user_playground
+/group/kfzv_db
+/group/cardservices_app
+/group/kfzv_app
+/middleware/middleware
+/middleware/wls
+/middleware/play
+/middleware/tomcat
+/middleware/oep
+/middleware/osb
+/os/i18n
+/os/pf_systeminfo
+/os/kvm
+/os/hardening
+/os/services
+/os/kickstart
+/tools/keyadm
+/tools/puppet_enc_demo
+/tools/splunk_masternode
+/tools/keybox
+/tools/splunkforwarder
+/tools/ansible
 `
 
 func TestCreate(t *testing.T) {
 	list := strings.Split(files, "\n")
 
 	deps := []Dependency{
-		{
-			Dir: "/ddd/fff/iii",
-			Dep: "/ccc/eee/iii",
-		},
-		{
-			Dir: "/ddd/hhh/jjj/llll",
-			Dep: "/ddd/hhh/jjj/mmmm",
-		},
+	// {
+	// Dir: "/application",
+	// Dep: "/os/",
+	// },
+
+	// {
+	// Dir: "/ddd/hhh/jjj/llll",
+	// Dep: "/ddd/hhh/jjj/mmmm",
+	// },
 	}
 
 	g := Create(list, deps)
@@ -125,5 +66,38 @@ func TestCreate(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to unmarshal %s: %s", "src", err)
 	}
+
+	order, cyclic := topSortDFS(*g)
 	fmt.Println(string(data))
+	fmt.Println(order)
+	fmt.Println(cyclic)
+}
+
+func TestBla(t *testing.T) {
+	data := `---
+- name: 2
+  edges: []
+- name: 5
+  edges: [11]
+- name: 11
+  edges: [2, 9, 10]
+- name: 7
+  edges: [11, 8]
+- name: 9
+  edges: []
+- name: 10
+  edges: []
+- name: 8
+  edges: [9]
+- name: 3
+  edges: [10, 8, 11]
+`
+	nodes := []Node{}
+	if err := yaml.Unmarshal([]byte(data), &nodes); err != nil {
+		t.Fatalf("failed to unmarshal %s: %s", "src", err)
+	}
+	fmt.Println(nodes)
+	l, err := bla(nodes)
+	fmt.Println(err)
+	fmt.Println(l)
 }
